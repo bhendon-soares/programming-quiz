fc33874cafb318f2744178b1af72a56686cc62ba
Bhendon Soares <bhendon.soares12@gmail.com>
2024-02-10 01:21:30 -0300

    feat: adiciona verificação das respostas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//Perguntas e respostas do quiz:
 const perguntas = [
     {
         pergunta: "O que é uma variável em programação?",
@@ -94,6 +95,11 @@ const perguntas = [
 const quiz = document.querySelector('#quiz');
 const template = document.querySelector('template');
 
+const corretas = new Set();
+const totalDePerguntas = perguntas.length;
+const mostrarTotal = document.querySelector('#acertos span');
+mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas;
+
 for (const item of perguntas) {
     const quizItem = template.content.cloneNode(true);
     quizItem.querySelector('h3').textContent = item.pergunta;
@@ -101,6 +107,21 @@ for (const item of perguntas) {
     for (let resposta of item.respostas) {
         const dt = quizItem.querySelector('dl dt').cloneNode(true);
         dt.querySelector('span').textContent = resposta;
+        dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item));
+        dt.querySelector('input').value = item.respostas.indexOf(resposta)
+        
+        //verificar alternativas:
+        dt.querySelector('input').onchange = (event) => {
+            const isCorrect = event.target.value == item.correta;
+
+            corretas.delete(item);
+            if (isCorrect) {
+                corretas.add(item);
+            }
+            
+            mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas;
+        }
+
         quizItem.querySelector('dl').appendChild(dt);
     }
 
